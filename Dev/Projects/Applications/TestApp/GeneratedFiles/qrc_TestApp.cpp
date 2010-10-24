d6bc9e6d01b2e597f23c7b2a962afcdba1efe2aa
ccanan <blancoding@gmail.com>
2010-10-24 21:45:53 +0800

    update shader path

--- Dev/Projects/Applications/TestApp/GeneratedFiles/qrc_TestApp.cpp
+++ Dev/Projects/Applications/TestApp/GeneratedFiles/qrc_TestApp.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Resource object code
 **
-** Created: Tue Sep 14 21:20:02 2010
+** Created: Sun Oct 24 21:42:09 2010
 **      by: The Resource Compiler for Qt version 4.7.0
 **
 ** WARNING! All changes made in this file will be lost!
