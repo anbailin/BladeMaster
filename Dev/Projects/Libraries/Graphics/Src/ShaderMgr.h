3283567d7763aaee60f5e0ffc1d7baeb3f7f530b
ccanan <blancoding@gmail.com>
2010-11-29 01:31:18 +0800

    add comments for shader mgr

--- Dev/Projects/Libraries/Graphics/Src/ShaderMgr.h
+++ Dev/Projects/Libraries/Graphics/Src/ShaderMgr.h
@@ -9,7 +9,9 @@ typedef u64 ShaderId;
 #define InvalidShaderMacroId 0x00000000ffffffff
 #define InvalidShaderId 0xffffffffffffffff
 
-//class 
+/*
+ *	store shader pair(vs,ps), and the intermediate data
+ */
 struct ShaderHandle
 {
     ShaderHandle():mPreprocessShaderCrc(0),mVSCompiledSrcCrc(0),mPSCompiledSrcCrc(0){} 
@@ -23,6 +25,9 @@ struct ShaderHandle
     u32              mPSCompiledSrcCrc;
 };
 
+/*
+ *	shader description, information stored in the corresponding xml files
+ */
 struct ShaderDesc
 {
     BMStr                   name;
@@ -43,37 +48,54 @@ public:
     ShaderMgr();
     ~ShaderMgr();
 
+    /*
+     *	load shaders from cache, 
+     *	if cache miss, recompile shader and update cache
+     */
     void LoadShaders();
+
+    /*
+     *	retrieve shader functions
+     */
     ShaderId GetShaderTypeId(const char* name)const;
     ShaderId GetShaderMacroMask(const char* typeName, const char* macroName)const;
-    ShaderId GetShaderMacroMask(const ShaderId& typeId, const char* macroName)const;
-    
+    ShaderId GetShaderMacroMask(const ShaderId& typeId, const char* macroName)const;    
     const ShaderHandle* GetShader(const ShaderId& id)const;
+
+    /*
+     *	apply shaders
+     */
     void ApplyShader(const ShaderHandle* shaderHandle)const;
     void ApplyShader(const ShaderId& shaderId)const;
-
-    bool CompileShader(
-        const wchar_t* path, 
-        const D3DXMACRO* defines, 
-        LPD3DXINCLUDE includes,         
-        DWORD flags,
-        PixelShaderPtr& pixeShader,
-        VertexShaderPtr& vertexShader,
-        TArray<u8>& vsCode,
-        TArray<u8>& psCode
-        );
-
-    bool CompileShader(ShaderHandle& shaderHandle, bool recordCompiledResult = false);
-    bool CreateShader(ShaderHandle& shaderHandle);
-    bool PreprocessShader(const wchar_t* path, const D3DXMACRO* defines, LPD3DXINCLUDE includes, ShaderHandle& shaderHandle);
+    
 
     static bool DetermineShaderAnimation(u32 _BoneNum);
     //utils
 protected:
+
+    /*
+     *	compile shader from preprocess shader source
+     */
+    bool CompileShader(ShaderHandle& shaderHandle, bool recordCompiledResult = false);
+    /*
+     *	create vs/ps from compiled shader src
+     */
+    bool CreateShader(ShaderHandle& shaderHandle);
+    /*
+     *	preprocess shader source with macro definitions
+     */
+    bool PreprocessShader(const wchar_t* path, const D3DXMACRO* defines, LPD3DXINCLUDE includes, ShaderHandle& shaderHandle);
+
+    /*
+     *	utils for parse shader descriptions
+     */
     void ParseShaderDesc(const BMStr& descPath, ShaderDesc& shaderDesc);
     void ParseShaderNation(TiXmlElement* shaderNation, ShaderDesc& shaderDesc);
     void ParseShaderProvince(TiXmlElement* shaderProvince, ShaderDesc& shaderDesc, std::vector<ShaderId>& ids);    
     
+    /*
+     *	update shader cache
+     */
     void LoadShaderCache();
     void UpdateShaderCache();
     //attr
